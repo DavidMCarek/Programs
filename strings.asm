9f9c164b221bf3aef0a88d81923658fb99a95784
DavidMCarek <dmcarek@yahoo.com>
2015-06-29 09:00:31 -0700

    changed some stuff

--- strings.asm
+++ strings.asm
@@ -1,6 +1,7 @@
 .Model  small
 .386
 .Stack  100h
+Include PCMAC.Inc
 .Data
     NewLine         DB 0dh, 0ah, '$'
     MaxStrLength    equ 51
@@ -9,18 +10,18 @@
                     DB 51 dup(?)
 
     GetNewInputMsg  DB 'Please enter a new string of characters > ', '$'
-    FunctionList    DB 'Please enter a number 1-9 to select a function', 0dh, 0ah,
-                     '1) Find the location of a characters first occurence', 0dh, 0ah,
-                     '2) Find the number of occurences of a character', 0dh, 0ah,
-                     '3) Find the length of the string', 0dh, 0ah,
-                     '4) Find the number of alpha numeric characters', 0dh, 0ah,
-                     '5) Replace every occurence of a letter with a different one', 0dh, 0ah,
-                     '6) Capitalize all letters in the string', 0dh, 0ah,
-                     '7) Lowercase all letter in the string', 0dh, 0ah,
-                     '8) Toggle the case of all letters', 0dh, 0ah,
-                     '9) Input a new String', 0dh, 0ah,
-                     'U) Undo the last function (only works once)', 0dh, 0ah,
-                     'E) Exit', 0dh, 0ah, '$'
+    SelectFMsg      DB 'Please enter a number 1-9 to select a function or 0 to list the functions', 0dh, 0ah, '$'
+    F1Msg   DB '1) Find the location of a characters first occurence', 0dh, 0ah, '$'
+    F2Msg   DB '2) Find the number of occurences of a character', 0dh, 0ah, '$'
+    F3Msg   DB '3) Find the length of the string', 0dh, 0ah, '$'
+    F4Msg   DB '4) Find the number of alpha numeric characters',0dh, 0ah, '$'
+    F5Msg   DB '5) Replace every occurence of a letter with a different one', 0dh, 0ah, '$'
+    F6Msg   DB '6) Capitalize all letters in the string', 0dh, 0ah, '$'
+    F7Msg   DB '7) Lowercase all letter in the string', 0dh, 0ah, '$'
+    F8Msg   DB '8) Toggle the case of all letters', 0dh, 0ah, '$'
+    F9Msg   DB '9) Input a new String', 0dh, 0ah, '$'
+    UndoMsg DB 'U) Undo the last function (only works once)', 0dh, 0ah, '$'
+    ExitMsg DB 'E) Exit', 0dh, 0ah, '$'
     CurrentString   DB '$'
     InvalidInputMsg DB 'The input entered was not a valid function', 0dh, 0ah, '$'
 
@@ -51,8 +52,7 @@ ExitProgram:
     SelectFunction Proc
     push dx
     mov ah, 09h
-    mov dx, offset FunctionList
-    int 21h
+    _PutStr SelectFMsg
 
     mov ah, 01h
     int 21h
@@ -65,6 +65,13 @@ ExitProgram:
     RunFunction Proc
     pushf
     push ax
+
+    cmp al, '0'
+    jne NotF0
+    call Function0
+    jmp FunctionRun
+    NotF0:
+
     cmp al, '1'
     jne NotF1
     call Function1
@@ -142,6 +149,11 @@ ExitProgram:
 
     ret
     RunFunction EndP
+
+    Function0 Proc
+
+    ret
+    Function0 EndP
 
     Function1 Proc
 
