829e4bf7c66e709fed38a6d42ccec69d070fafc4
DavidMCarek <dmcarek@yahoo.com>
2015-07-21 12:09:23 -0700

    its broken

--- radix.asm
+++ radix.asm
@@ -13,6 +13,18 @@ Include PCMAC.Inc
     InputMsg                DB  'Please enter a value in the selected radix between -32768 and 32767 decimal > ', '$'
     RunAgain                DB  'Would you like to run again? (y/Y or n/N) > ', '$'
     InvalidInputMsg         DB  'Invalid input', '$'
+    BackSpaceCounter        DW  0
+    Addition                DB  'A + B = ', '$'
+    Subtraction             DB  'A - B = ', '$'
+    Multiplication          DB  'A ', 0F9h, ' B = ', '$'
+    Division                DB  'A ', 0F6h, ' B = ', '$'
+    Power                   DB  'A ^ B = ', '$'
+    Decimal                 DB  ' Decimal ', '$'
+    Base                    DB  ' Base ', '$'
+    Comma                   DB  ', ', '$'
+    Quotient                DB  ' Quotient, ', '$'
+    Remainder               DB  ' Remainder ', '$'
+    StackCounter            DW  0
     
 .Code
     Radix   Proc
@@ -27,15 +39,18 @@ Include PCMAC.Inc
     InvalidChar:
         _PutStr RunAgain
         _GetCh
-        cmp al, 'y'
+        mov bl, al
+        _PutStr NewLine
+        cmp bl, 'y'
         je GetNewInputs
-        cmp al, 'Y'
+        cmp bl, 'Y'
         je GetNewInputs
-        cmp al, 'n'
+        cmp bl, 'n'
         je UserDone
-        cmp al, 'N'
+        cmp bl, 'N'
         je UserDone
 
+        _PutStr NewLine
         _PutStr InvalidInputMsg
         _PutStr NewLine
         jmp InvalidChar
@@ -94,6 +109,35 @@ Include PCMAC.Inc
     GetNextCharacter:
         _GetCh  noEcho
 
+        cmp al, 8
+        jne NotBackSpace
+        cmp BackSpaceCounter, 0
+        je SkipErase
+
+        mov dl, 08h
+        _PutCh 
+        mov dl, 00h
+        _PutCh
+        mov dl, 08h
+        _PutCh
+        dec BackSpaceCounter
+
+    SkipErase:
+        cmp bx, 0
+        jne DontCheckS
+        cmp si, 'S'
+        je DontCheckS
+        xor si, si
+
+    DontCheckS:
+        mov ax, bx
+        xor dx, dx
+        div cx
+        mov bx, ax
+        jmp GetNextCharacter
+
+    NotBackSpace:
+
         cmp si, 'S'
         je NegNotNeeded
 
@@ -105,50 +149,68 @@ Include PCMAC.Inc
     NegNotNeeded:
         cmp si, 'S'
         jne NoSpecialCharacters
+
         cmp al, 'B'
         jne NotCapB
         mov bl, 2
+        mov dl, al
+        _PutCh
+        jmp GetNextCharacter
 
     NotCapB:
         cmp al, 'b'
         jne NotLowB
         mov bl, 2
+        mov dl, al
+        _PutCh
         jmp GetNextCharacter
 
     NotLowB:
         cmp al, 'O'
         jne NotCapO
         mov bl, 8
+        mov dl, al
+        _PutCh
         jmp GetNextCharacter
 
     NotCapO:
         cmp al, 'o'
         jne NotLowO
         mov bl, 8
+        mov dl, al
+        _PutCh
         jmp GetNextCharacter
 
     NotLowO:
         cmp al, 'D'
         jne NotCapD
         mov bl, 10
+        mov dl, al
+        _PutCh
         jmp GetNextCharacter
 
     NotCapD:
         cmp al, 'd'
         jne NotLowD
         mov bl, 10
+        mov dl, al
+        _PutCh
         jmp GetNextCharacter
 
     NotLowD:
         cmp al, 'H'
         jne NotCapH
         mov bl, 16
+        mov dl, al
+        _PutCh
         jmp GetNextCharacter
 
     NotCapH:
         cmp al, 'h'
         jne NoSpecialCharacters
         mov bl, 16
+        mov dl, al
+        _PutCh
         jmp GetNextCharacter
 
     NoSpecialCharacters:
@@ -156,21 +218,6 @@ Include PCMAC.Inc
         cmp al, 0Dh
         je LastCharEntered
 
-        cmp al, 8
-        jne NotBackSpace
-        mov dl, 08h
-        _PutCh 
-        mov dl, 00h
-        _PutCh
-        mov dl, 08h
-        _PutCh
-        mov ax, bx
-        xor dx, dx
-        div cx
-        mov bx, ax
-        jmp GetNextCharacter
-
-    NotBackSpace:
 
         cmp al, '0'
         jb GetNextCharacter
@@ -179,8 +226,6 @@ Include PCMAC.Inc
 
         mov dl, al
         sub al, '0'
-        cmp al, cl
-        jge GetNextCharacter
         jmp AddToValue
 
     CheckIfCap:
@@ -191,8 +236,6 @@ Include PCMAC.Inc
 
         mov dl, al
         sub al, 37h
-        cmp al, cl
-        jge GetNextCharacter
         jmp AddToValue
 
     CheckIfLow:
@@ -203,18 +246,22 @@ Include PCMAC.Inc
 
         mov dl, al
         sub al, 57h
-        cmp al, cl
-        jge GetNextCharacter
 
     AddToValue:
+        cmp al, cl
+        jge GetNextCharacter
         xchg ax, bx
+        push dx
+        xor dx, dx
         mul cx
         add bx, ax
+        pop dx
         _PutCh
+        inc BackSpaceCounter
         jmp GetNextCharacter
 
     LastCharEntered:
-        mov bx, ax
+        mov ax, bx
         cmp si, 01h
         jne DoNotNeg
         neg ax
@@ -231,15 +278,191 @@ Include PCMAC.Inc
 ;/////////////////////////////////////////////////////////
 
     DoTheMath   Proc
+    
+        ;Addition
+        _PutStr Addition
+        mov ax, InputA
+        add ax, InputB
 
-    ret
+        mov bx, ax
+        mov cx, OutputRadix
+        call PutRadix
+        _PutStr Base
+        mov cx, 0Ah
+        mov ax, OutputRadix
+        call PutRadix
+        _PutStr Comma
+        mov ax, bx
+        call PutRadix
+        _PutStr Decimal
+        _PutStr NewLine
+    
+        ;Subtraction
+        _PutStr Subtraction
+        mov ax, InputA
+        sub ax, InputB
+
+        mov bx, ax
+        mov cx, OutputRadix
+        call PutRadix
+        _PutStr Base
+        mov cx, 0Ah
+        mov ax, OutputRadix
+        call PutRadix
+        _PutStr Comma
+        mov ax, bx
+        call PutRadix
+        _PutStr Decimal
+        _PutStr NewLine
+    
+        ;Multiplication
+        _PutStr Multiplication
+        mov ax, InputA
+        mov bx, InputB
+        imul bx
+    
+        mov bx, ax
+        mov cx, OutputRadix
+        call PutRadix
+        _PutStr Base
+        mov cx, 0Ah
+        mov ax, OutputRadix
+        call PutRadix
+        _PutStr Comma
+        mov ax, bx
+        call PutRadix
+        _PutStr Decimal
+        _PutStr NewLine
+
+        ;Division
+        _PutStr Division
+        mov ax, InputA
+        mov bx, InputB
+        cwd 
+        idiv bx
+        push ax
+        push dx
+
+        mov cx, OutputRadix
+        call PutRadix
+        _PutStr Quotient 
+        pop ax
+        mov bx, ax
+        call PutRadix
+        _PutStr Remainder
+        _PutStr Base
+        mov cx, 0Ah
+        mov ax, OutputRadix
+        call PutRadix
+        _PutStr Comma
+        pop ax
+        call PutRadix
+        _PutStr Quotient
+        mov ax, bx
+        call PutRadix
+        _PutStr Remainder
+        _PutStr Decimal
+        _PutStr NewLine
+
+        ;Power
+        _PutStr Power
+        mov ax, InputA
+        mov bx, InputA
+        mov cx, InputB
+        xor dx, dx
+
+    PowerLoop:
+        dec cx
+        cmp cx, 0
+        je DoneWithPower
+        mul bx
+        jmp PowerLoop
+        
+    DoneWithPower:
+
+        mov bx, ax
+        mov cx, OutputRadix
+        call PutRadix
+        _PutStr Base
+        mov cx, 0Ah
+        mov ax, OutputRadix
+        call PutRadix
+        _PutStr Comma
+        mov ax, bx
+        call PutRadix
+        _PutStr Decimal
+        _PutStr NewLine
+
+        ret
     DoTheMath   EndP
 
 ;/////////////////////////////////////////////////////////
 
     PutRadix    Proc
 
-    ret
+        ;cx has output radix
+        ;ax has value to print
+        push bx
+        push dx
+        push si
+        pushf
+
+        mov StackCounter, 0
+
+        cmp ax, 0
+        jge OutputIsPositive
+        mov si, 1
+        neg ax
+
+    OutputIsPositive:
+        xor dx, dx
+        div cx
+
+        push ax
+        push dx
+        _PutCh
+        pop dx
+        pop ax
+
+        push dx
+        inc StackCounter
+        cmp ax, 0
+        je ValueConverted
+        jmp OutputIsPositive
+
+    ValueConverted:
+        mov cx, StackCounter
+        cmp si, 1
+        jne PrintLoop
+        mov dl, '-'
+        _PutCh
+        xor dx, dx
+        
+    PrintLoop:
+        pop dx
+        dec cx
+        
+        cmp dx, 9
+        jg LetterOutput
+        add dx, '0'
+        _PutCh
+        jcxz PrintFinish 
+        jmp PrintLoop
+
+    LetterOutput:
+        add dx, 37h
+        _PutCh
+        jcxz PrintFinish 
+        jmp PrintLoop
+
+    PrintFinish:
+
+        popf
+        pop si 
+        pop dx
+        pop bx
+
+        ret
     PutRadix    EndP
 
 ;/////////////////////////////////////////////////////////
